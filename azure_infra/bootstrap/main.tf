4466b4aa509cd4080eda6ce81388b431fd3477f6
Your Name <you@example.com>
2026-09-18 20:11:37 +0530

    DEVOPS_TICKET_13_testv4

--- azure_infra/bootstrap/main.tf
+++ azure_infra/bootstrap/main.tf
@@ -38,7 +38,7 @@
   resource "azurerm_container_registry" "acr" {
     name                = var.acr_name
     resource_group_name = azurerm_resource_group.RG_ACR.name
-    location            = azurerm_resource_group.acr.location
+    location            = azurerm_resource_group.RG_ACR.location
     sku                 = var.acr_sku
 
   
